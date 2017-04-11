96a2b87ef8b1ee292a7288bed161a347a0c235e4
chrnndz3 <chrnndz3@gmail.com>
2017-04-11 15:47:27 -0500

    initial commit

---
+++ AndroidApp/app/src/main/java/com/example/twiganator/androidapp/Display.java
@@ -0,0 +1,17 @@
+package com.example.twiganator.androidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by twiganator on 4/11/17.
+ */
+
+public class Display extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+    }
+}
---
+++ AndroidApp/app/src/main/java/com/example/twiganator/androidapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.twiganator.androidapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v){
+        if(v.getId() == R.id.signIn){
+            Intent i = new Intent(MainActivity.this, Display.class);
+            startActivity(i);
+        }
+    }
+}
